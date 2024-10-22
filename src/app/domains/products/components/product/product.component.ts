1d0b6d051b5b9d9abbf059017b3e2d8666ff9b78
edusebass <edu03sebas@gmail.com>
2024-10-21 23:10:53 -0500

    Creando componente de productos 42

--- src/app/domains/products/components/product/product.component.ts
+++ src/app/domains/products/components/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Product } from '../../../shared/models/product.model';
 
 @Component({
   selector: 'app-product',
@@ -10,16 +11,15 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class ProductComponent {
   // img = 'https://picsum.photos/340/340?r=' + Math.random();
 
-  @Input() img: string = '';
-  @Input() price: number = 0;
-  @Input({ required: true }) title: string = '';
+  @Input({ required: true }) product!: Product;
 
   @Output() addToCart = new EventEmitter();
 
   addToCartHandler() {
     console.log('click from child');
-    this.addToCart.emit(
-      'holi esto es un mdg desde el hijo pi.assaasasd' + this.title,
-    );
+    this.addToCart
+      .emit
+      // 'holi esto es un mdg desde el hijo pi.assaasasd' + this.title,
+      ();
   }
 }
--- src/app/domains/products/pages/list/list.component.ts
+++ src/app/domains/products/pages/list/list.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ProductComponent } from './../../components/product/product.component';
 import { CommonModule } from '@angular/common';
+import { Product } from '../../../shared/models/product.model';
 
 @Component({
   selector: 'app-list',
@@ -10,6 +11,20 @@ import { CommonModule } from '@angular/common';
   styleUrl: './list.component.css',
 })
 export class ListComponent {
+  products = signal<Product[]>([]);
+
+  constructor() {
+    const initProducts: Product[] = [
+      {
+        id: Date.now(),
+        title: 'Product 1',
+        price: 100,
+        img: 'https://picsum.photos/100/100?r=2',
+        creationAt: new Date().toISOString(),
+      },
+    ];
+    this.products.set(initProducts);
+  }
   fromChild(event: string) {
     console.log(event);
     console.log('des list compnent');
